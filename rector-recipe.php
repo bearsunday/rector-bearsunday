1b1435f68ae7999d3fdac4e97e229d0e147c7bb0
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-03-29 11:18:07 +0900

    Create recipe

--- rector-recipe.php
+++ rector-recipe.php
@@ -14,12 +14,12 @@ return static function (\Symfony\Component\DependencyInjection\Loader\Configurat
     $rectorRecipeConfiguration = [
         // [RECTOR CORE CONTRIBUTION - REQUIRED]
         // package name, basically namespace part in `rules/<package>/src`, use PascalCase
-        \Rector\RectorGenerator\ValueObject\Option::PACKAGE => 'RayNamedAnnotation',
+        \Rector\RectorGenerator\ValueObject\Option::PACKAGE => 'RayDiNamedAnnotation',
         // name, basically short class name; use PascalCase
-        \Rector\RectorGenerator\ValueObject\Option::NAME => 'RayNamedAnnotationRector',
+        \Rector\RectorGenerator\ValueObject\Option::NAME => 'RayDiNamedAnnotationRector',
         // 1+ node types to change, pick from classes here https://github.com/nikic/PHP-Parser/tree/master/lib/PhpParser/Node
         // the best practise is to have just 1 type here if possible, and make separated rule for other node types
-        \Rector\RectorGenerator\ValueObject\Option::NODE_TYPES => [\PhpParser\Node\Stmt\Class_::class, \PhpParser\Node\Stmt\ClassMethod::class, \PhpParser\Node\Stmt\Property::class, \PhpParser\Node\Stmt\Interface_::class, \PhpParser\Node\Param::class, \PhpParser\Node\Stmt\Function_::class],
+        \Rector\RectorGenerator\ValueObject\Option::NODE_TYPES => [ \PhpParser\Node\Stmt\ClassMethod::class],
         // describe what the rule does
         \Rector\RectorGenerator\ValueObject\Option::DESCRIPTION => '"Mehtod @named annotation will changed to be parameter #[Named] attribute"',
         // code before change
@@ -39,12 +39,15 @@ CODE_SAMPLE
 ,
         // code after change
         \Rector\RectorGenerator\ValueObject\Option::CODE_AFTER => <<<'CODE_SAMPLE'
+class SomeClass
+{
     /**
      * @Foo
      */
-    public function __construct(#[Named('foo') int $a, #[Named('bar') int $b)
+    public function __construct(#[\Ray\Di\Di\Named('foo')] int $a, #[\Ray\Di\Di\Named('bar')] int $b)
     {
     }
+}
 CODE_SAMPLE
 ,
     ];
