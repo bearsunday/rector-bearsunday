359a772df8999eda3140bb1eb6105ca81056635e
Akihito Koriyama <akihito.koriyama@gmail.com>
2023-09-25 09:46:46 +0900

    Added annotation for BEAR.Sunday and related projects

--- rector.php
+++ rector.php
@@ -2,19 +2,87 @@
 
 declare(strict_types=1);
 
+use BEAR\Package\Annotation\ReturnCreatedResource;
+use BEAR\RepositoryModule\Annotation\Cacheable;
+use BEAR\RepositoryModule\Annotation\Purge;
+use BEAR\RepositoryModule\Annotation\Refresh;
+use BEAR\Resource\Annotation\AppName;
+use BEAR\Resource\Annotation\ContextScheme;
+use BEAR\Resource\Annotation\Embed;
+use BEAR\Resource\Annotation\ImportAppConfig;
+use BEAR\Resource\Annotation\Link;
+use BEAR\Resource\Annotation\OptionsBody;
+use BEAR\Resource\Annotation\ResourceParam;
+use Ray\AuraSqlModule\Annotation\ReadOnlyConnection;
+use Ray\AuraSqlModule\Annotation\Transactional;
+use Ray\AuraSqlModule\Annotation\WriteConnection;
+use Ray\Di\Di\Assisted;
+use Ray\Di\Di\Inject;
+use Ray\Di\Di\Named;
+use Ray\Di\Di\PostConstruct;
+use Ray\Di\Di\Qualifier;
+use Ray\Di\Di\Set;
+use Ray\PsrCacheModule\Annotation\CacheDir;
+use Ray\PsrCacheModule\Annotation\CacheNamespace;
+use Ray\PsrCacheModule\Annotation\Local;
+use Ray\PsrCacheModule\Annotation\Shared;
+use Ray\Query\Annotation\Query;
+use Ray\RoleModule\Annotation\RequiresRoles;
+use Ray\WebContextParam\Annotation\CookieParam;
+use Ray\WebContextParam\Annotation\EnvParam;
+use Ray\WebContextParam\Annotation\FormParam;
+use Ray\WebContextParam\Annotation\QueryParam;
+use Ray\WebContextParam\Annotation\ServerParam;
 use Rector\BearSunday\RayDiNamedAnnotation\Rector\ClassMethod\RayDiNamedAnnotationRector;
+use Rector\Config\RectorConfig;
 use Rector\Core\Configuration\Option;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
+use Rector\Php80\ValueObject\AnnotationToAttribute;
 
 require __DIR__ . '/vendor/autoload.php';
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    // get parameters
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [
-        __DIR__ . '/src'
+return static function (RectorConfig $rectorConfig): void {
+
+    $rectorConfig->ruleWithConfiguration(AnnotationToAttributeRector::class, [
+        // ray/aura-sql-module
+        new AnnotationToAttribute(ReadOnlyConnection::class),
+        new AnnotationToAttribute(WriteConnection::class),
+        new AnnotationToAttribute(Transactional::class),
+
+        // ray/di
+        new AnnotationToAttribute(Assisted::class),
+        new AnnotationToAttribute(Inject::class),
+        new AnnotationToAttribute(Named::class),
+        new AnnotationToAttribute(PostConstruct::class),
+        new AnnotationToAttribute(Set::class),
+        new AnnotationToAttribute(Qualifier::class),
+        // bear/query-repository
+        new AnnotationToAttribute(Cacheable::class),
+        new AnnotationToAttribute(Purge::class),
+        new AnnotationToAttribute(Refresh::class),
+        // ray/psr-cache-module
+        new AnnotationToAttribute(CacheNamespace::class),
+        new AnnotationToAttribute(Local::class),
+        new AnnotationToAttribute(Shared::class),
+        new AnnotationToAttribute(CacheDir::class),
+        new AnnotationToAttribute(AppName::class),
+        new AnnotationToAttribute(ContextScheme::class),
+        // bear/resource
+        new AnnotationToAttribute(Embed::class),
+        new AnnotationToAttribute(ImportAppConfig::class),
+        new AnnotationToAttribute(Link::class),
+        new AnnotationToAttribute(OptionsBody::class),
+        new AnnotationToAttribute(ResourceParam::class),
+        // ray/web-context
+        new AnnotationToAttribute(CookieParam::class),
+        new AnnotationToAttribute(EnvParam::class),
+        new AnnotationToAttribute(\Ray\WebContextParam\Annotation\FilesParam::class),
+        new AnnotationToAttribute(FormParam::class),
+        new AnnotationToAttribute(QueryParam::class),
+        new AnnotationToAttribute(ServerParam::class),
+        new AnnotationToAttribute(ReturnCreatedResource::class),
+
     ]);
 
-    $services = $containerConfigurator->services();
-    $services->set(RayDiNamedAnnotationRector::class);
+    $rectorConfig->sets([RayDiNamedAnnotationRector::class]);
 };
